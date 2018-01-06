ec780333271fb83920abb485df2b56a29aea1149
hajji tarek <hajji-tarik@outlook.fr>
2018-01-06 05:07:38 +0000

    rewrite all plugin

--- index.js
+++ index.js
@@ -1,33 +1,82 @@
-function HtmlPlugin(options) {
-  this.options = options;
+"use strict";
+import path from 'path';
+
+class FullHTMLPlugin {
+  constructor(options) {
+    if (arguments.length > 1) {
+      throw new Error(`Please check you options and be sure to pass the correct params`);
+    }
+
+    this.options = { ...options };
+  }
+
+  jsHandler = (js) => {
+    const _js = js instanceof Array ? js : [js];
+    let tmp = "";
+    console.log(_js.length);
+    if (_js.length === 0) return html;
+    for (let i = 0; i < _js.length; i++) {
+      tmp += '<script type="text/javascript" src="' + _js[i] + '"></script>\n';
+    }
+    return tmp;
+  };
+
+  cssHandler = (css, html) => {
+    const _css = css instanceof Array ? css : [css];
+    let tmp = "";
+    if (_css.length === 0) return html;
+    for (let i = 0; i < _css.length; i++) {
+      tmp += '<link rel="stylesheet" type="text/css" href="' + _css[i] + '"/>\n';
+    }
+    return tmp;
+  };
+
+  initialTemplate = ({ title, classname, css, js }, cssHandler, jsHandler) => {
+    let html = '<!DOCTYPE html>\n';
+    html += '<html>\n';
+    html += '<head>\n';
+    html += '<meta charset="UTF-8">\n';
+    html += '<title>' + title + '</title>\n';
+
+    html += cssHandler(css);
+
+    html += '</head>\n';
+    html += '<body>\n';
+    html += '<div class="' + classname + '"></div>\n';
+
+    html += jsHandler(js);
+
+    html += '</body>\n';
+    html += '</html>';
+
+    return html;
+  };
+
+
+  apply(compiler) {
+    const { options, cssHandler, jsHandler } = this;
+    const filename = options.filename;
+    let file = null;
+    if (path.resolve(filename) === path.normalize(filename)) {
+      options.filename = path.relative(compiler.options.output.path, filename);
+    }
+
+    compiler.plugin("emit", (compilation, callback) => {
+
+      file = this.initialTemplate(options, cssHandler, jsHandler);
+      compilation.assets[this.options.filename] = {
+        source: function () {
+          return file;
+        },
+        size: function () {
+          return file.length;
+        }
+      };
+
+      callback();
+
+    });
+  }
 }
 
-HtmlPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('emit', (compilation, callback) => {
-    var html = '<!DOCTYPE html>\n';
-    html+= '<html>\n';
-    html+= '<head>\n';
-    html+= '<meta charset="UTF-8">\n';
-    html+= '<title>'+this.options.title+'</title>\n';
-    html+= '<link rel="stylesheet" type="text/css" href="'+this.options.css+'"/>\n';
-    html+= '</head>\n';
-    html+= '<body>\n';
-    html+= '<div class="'+this.options.className+'"></div>\n';
-    html+= '<script type="text/javascript" src="'+this.options.js+'"></script>\n';
-    html+= '</body>\n';
-    html+= '</html>';
-
-    compilation.assets[this.options.filename] = {
-      source: function() {
-        return html;
-      },
-      size: function() {
-        return html.length;
-      }
-    };
-
-    callback();
-  });
-};
-
-module.exports = HtmlPlugin;
+module.exports = FullHTMLPlugin;
